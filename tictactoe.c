fdbfb7c26e6e3be0b6a9abd17fd7ee87cef8f3ac
SatyaRajAwasth1 <awasthisatyaraj2060@gmail.com>
2022-07-04 22:42:54 +0545

    |   | ---|---|---    |   | ---|---|---    |   |

--- tictactoe.c
+++ tictactoe.c
@@ -42,12 +42,12 @@ void printBoard(){
    |   |  
 */
 
+printf("  %c | %c  | %c ", board[0][0],board[0][1], board[0][2]);
+printf("\n---|---|---\n");
 
-   printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
-   printf("\n---|---|---\n");
-   printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
-   printf("\n---|---|---\n");
-   printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+printf("  %c | %c  | %c ", board[1][0],board[1][1], board[1][2]);
+printf("\n---|---|---\n");
 
+printf("  %c | %c  | %c ", board[2][0],board[2][1], board[2][2]);
    printf("\n");
 }
